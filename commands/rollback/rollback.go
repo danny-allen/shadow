042bc63480e91165f02a7d20623930433fd7f0d6
dallen <danny@wzukltd.com>
2016-12-22 17:37:22 +0000

    Started rollback.

--- commands/rollback/rollback.go
+++ commands/rollback/rollback.go
@@ -1,7 +1,40 @@
 package rollback
 
-import "dao/shadow/config"
+import (
+	"dao/shadow/config"
+	"os"
+	"fmt"
+)
 
 func Run(Cfg *config.Config) {
+
+	// Check args for version.
+	if(len(os.Args) > 2) {
+
+		// Get the required version by the user.
+		requiredVersion := os.Args[2]
+
+		// If no arg, find version from last available.
+		version := Cfg.Version.GetLast()
+
+		// Check the required version is not already the current.
+		if(version == Cfg.Version.GetCurrent()) {
+
+			// Tell the user it is!
+			fmt.Println("You are already on the latest version")
+		} else {
+
+			// If arg, try and find that version from update log.
+			if(Cfg.Version.FindByTag(requiredVersion)) {
+
+			} else {
+
+			}
+		}
+
+	} else {
+
+	}
+
 
 }
