d0795b5f7fc88499a026c3fef24928bbf8b95523
dallen <danny@wzukltd.com>
2017-01-13 13:01:13 +0000

    Config now neater and merges global and local configs.

--- library/config.go
+++ library/config.go
@@ -5,10 +5,8 @@ import (
 	"os"
 	"gopkg.in/ini.v1"
 	"dao/shadow/path"
+	"github.com/danny-allen/go-stop"
 )
-
-
-var Cfg *Config
 
 
 // Config data structure.
@@ -17,153 +15,218 @@ type Config struct {
 	ShadowFile 			*ini.File
 	UpdateLogPath		string
 	Version 			*version.Version
+	GlobalPath 			string
 }
 
-var localShadow *ini.File
-var downloadUrl	string
+// Configuration struct reference.
+var Cfg *Config
+
+// Shadow ini file reference.
+var shadowFile *ini.File
 
 
-
-// Set up configs.
+/**
+ * Setup configs.
+ */
 func Setup() {
+	/*
+		Does it have to be installed globally? no
+		Can you install files for global use, regardless of where the binary is installed? yes
+		Then global directory and folders should not need to be created, only on actions where they are required.
+		The config should no where this dir is though.
+	 */
 
 	// Create instance of config.
 	Cfg = NewConfig()
+
+	// Get the shadow file.
+	Cfg.ShadowFile = GetShadowFile()
+
+	// Make the global config.
+	//MakeGlobal()
+
+}
+
+
+/**
+ * Make the global structure.
+ */
+func MakeGlobal() {
+
+	// Set the global path.
+	Cfg.SetGlobalPath("/usr/local/shadow")
+
+	// Make the global directory.
+	MakeGlobalDirStructure()
+
+	// Create the shadow file.
+	CreateGlobalShadowFile();
+}
+
+
+/**
+ * Check if there is a global configuration.
+ */
+func IsGlobal() bool {
+
+	// Get exists result.
+	exists, _ := path.Exists("/usr/local/shadow/.shadow")
+
+	// Return it.
+	return exists
+}
+
+
+/**
+ * Set the global path on the config struct.
+ */
+func (c *Config) SetGlobalPath(path string) {
+
+	// Add global path to config.
+	c.GlobalPath = path
+}
+
+
+/**
+ * Create the global shadow file (if needed).
+ */
+func CreateGlobalShadowFile() {
+
+	// Get exists result.
+	exists, err := path.Exists(Cfg.GlobalPath + "/.shadow")
+
+	// Check if needs to be made first.
+	if(!exists) {
+
+		// Create the shadow file.
+		_, err := os.Create(".shadow")
+
+		// Throw error if needed.
+		if(err != nil ) {
+			stop.Fatal(err.Error())
+		}
+	}
+
+	// Throw error if needed.
+	if(err != nil ) {
+		stop.Fatal(err.Error())
+	}
+}
+
+
+/**
+ * Make the global directory structure.
+ */
+func MakeGlobalDirStructure() {
+
+	// Declare error var, in case...
+	var err error
+
+	// Get exists result.
+	exists, _ := path.Exists(Cfg.GlobalPath + "/.shadow_templates/")
+
+	// Check if the path exists already.
+	if(!exists) {
+
+		// Make the directory.
+		err = os.Mkdir(Cfg.GlobalPath + "/.shadow_templates/", 0755)
+
+		// Throw error if needed.
+		if (err != nil ) {
+
+			// Stop! Something went wrong.
+			stop.Fatal(err.Error())
+		}
+	}
+}
+
+
+/**
+ * Create the config struct and populate it.
+ */
+func NewConfig() *Config {
+
+	// Get the current path.
+	currentPath, _ := os.Getwd()
+
+	// Add values to new struct.
+	conf := &Config{
+		UpdateLogPath: "http://prj.noise-maker.co.uk/shadow",
+		CurrentPath: currentPath,
+		GlobalPath: "/usr/local/shadow",
+		Version: &version.Version{},
+	}
+
+	// Set the version tag.
+	conf.Version.SetTag("v0.0.2")
+
+	// Set the URL for the version log.
+	conf.Version.SetLogUrl("https://raw.githubusercontent.com/danny-allen/shadow/master/shadow_history.yaml")
 
 	/*
 		// Version.
 		ver := version.NewLog()
 		ver.Populate("https://raw.githubusercontent.com/danny-allen/shadow/master/shadow_history.yaml")
 		Cfg.Version = ver
-
-
-
 	 */
-
-	// Create new instance of version data.
-	Cfg.Version = &version.Version{}
-
-	tag := "v0.0.2"
-
-	// Set the version tag.
-	Cfg.Version.SetTag(tag)
-
-	// Set the URL for the version log.
-	Cfg.Version.SetLogUrl("https://raw.githubusercontent.com/danny-allen/shadow/master/shadow_history.yaml")
-
-
-	// Get local configuration file.
-	// Get global configuration file.
-	// Merge, prioritising
-
-	//cfg, err := ini.LooseLoad(".shadow")
-	//if err != nil {
-	//	fmt.Println(err.Error())
-	//	os.Exit(1)
-	//}
-	//
-	//fmt.Println(cfg)
-
-	//f, _ := os.Create("conf/goaway.conf")
-	//cfg.WriteTo(f)
-}
-
-
-
-
-
-func init() {
-	localShadow = ini.Empty()
-}
-
-func NewConfig() *Config {
-
-	// Get the config struct values.
-	currentPath := GetCurrentPath();
-	shadowFile := GetShadowFile();
-
-	// Add values to new struct.
-	conf := &Config{
-		CurrentPath: currentPath,
-		ShadowFile: shadowFile,
-		UpdateLogPath: "http://prj.noise-maker.co.uk/shadow",
-	}
 
 	// Return the config.
 	return conf
 }
 
+
+/**
+ * Get the shadow file. Could be from local, global or a mixture of both.
+ * When both are found, they are merged and the local is prioritised.
+ */
 func GetShadowFile() *ini.File {
 
-	/*
-		local := conf.Load(localPath)
-		global := conf.Load(globalPath)
-		config := conf.Merge(global, local)
-	*/
+	// Define the shadow ini file.
+	shadowFile = ini.Empty()
+
+	// Declare paths of shadow files.
+	localPath := ".shadow"
+	globalPath := Cfg.GlobalPath + "/.shadow"
 
 	// Default localShadow value.
-	localShadow = nil
+	var err error
 
 	// Check show file exists.
-	file, _ := path.Exists(".shadow")
+	localFile, _ := path.Exists(".shadow")
 
-	// If there is a file.
-	if(file) {
+	// Check show file exists.
+	globalFile, _ := path.Exists(Cfg.GlobalPath + "/.shadow")
 
-		// Load the local shadow file, worry about global later.
-		file, err := ini.Load(".shadow")
+	// Load the shadow file - look for the true statement.
+	switch(true){
 
-		//localShadow.
-		localShadow = file
+		// Load the shadow config (combined - local values are priority).
+		case globalFile && localFile:
+			shadowFile, err = ini.Load(globalPath, localPath)
+			break
 
-		// Panic on error.
-		if(err != nil){
-			panic(err);
-		}
+		// Load the local file only.
+		case localFile:
+			shadowFile, err = ini.Load(localPath)
+			break
+
+		// Load the global file only.
+		case globalFile:
+			shadowFile, err = ini.Load(globalPath)
+			break
+
+		// No shadow file.
+		default:
+			err = nil
+			shadowFile = nil
+			break
 	}
 
-	// Return the local shadow file.
-	return localShadow
-}
+	// Panic on error.
+	if(err != nil){
+		panic(err);
+	}
 
-func GetCurrentPath() string {
-
-	// Get current path.
-	currentPath, _ := os.Getwd()
-
-
-	return currentPath
-}
-
-// Loop configs.
-// If first config, add to base.
-// Else
-// Loop values
-// If key contains array/slice,
-// 	-
-
-func (c Config) GetByKey(key string) {
-
-}
-
-// Merges two configs.
-func Merge(configs ...map[string]*ini.Section) {
-
-//result := map[string]string{}
-//
-//for i, v := range configs {
-//
-//	// First Config
-//	if(i == 0) {
-//
-//		// Just set it, we wan't everything.
-//		result = v
-//
-//	} else {
-//
-//		// Loop the config properties and persist them.
-//		for confKey, confValue := range v {
-//			result[confKey] = confValue
-//		}
-//	}
+	// Return the shadow file.
+	return shadowFile
 }
